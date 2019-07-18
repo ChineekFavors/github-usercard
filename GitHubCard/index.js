8515e255e40270aa371801973659c2c6c0bd1b18
Chineek Favors <chineekfavors01@gmail.com>
2019-07-18 14:14:36 -0700

    add array of gitHub users

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,11 +6,11 @@ const axiosPromise = axios.get('https://api.github.com/users/ChineekFavors');
 
 axiosPromise
   .then(response => {
-      console.log(response.data);
+      //console.log(response.data);
       componentCreator(response);
   })
   .catch(error => {
-      cosole.log(`error: ${error}`);
+      console.log(`error: ${error}`);
   })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -20,17 +20,18 @@ axiosPromise
    Skip to Step 3.
 */
 
-  //reference to empty div with class name cards to place cards in
-const cards = document.querySelector('.cards');
 
 //function creates and return card      
 function componentCreator(response){
+  //reference to empty div with class name cards to place cards in
+const cards = document.querySelector('.cards');
   
   //created elements
   const card = document.createElement('div');
   const img = document.createElement('img');
-  const name = document.createElement('h1');
-  const userName = document.createElement('h2');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
   const location = document.createElement('p');
   const profile = document.createElement('p');
   const followers = document.createElement('p');
@@ -39,6 +40,7 @@ function componentCreator(response){
   // add class name to elements
   card.classList.add('card');
   img.classList.add('img');
+  cardInfo.classList.add('card-info');
   name.classList.add('name');
   userName.classList.add('username');
   location.classList.add('p');
@@ -47,7 +49,8 @@ function componentCreator(response){
   following.classList.add('p');
   bio.classList.add('p');
     // setting content for elements
-  img.src =  "https://avatars3.githubusercontent.com/u/45578867?v=4";
+    
+  img.src =  `${response.data.avatar_url}`;
   name.textContent = `${response.data.name}`;
   userName.textContent = `${response.data.login}`;
   location.textContent = `Location: ${response.data.location}`;
@@ -57,7 +60,8 @@ function componentCreator(response){
   bio.textContent = `Bio: ${response.data.bio}`;
   // appending
   card.appendChild(img);
-  card.appendChild(name);
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name);
   name.appendChild(userName);
   userName.appendChild(location);
   location.appendChild(profile);
@@ -82,7 +86,16 @@ function componentCreator(response){
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell','ubermoc'];
+followersArray.forEach(string => { 
+  axios.get(`https://api.github.com/users/${string}`)
+    .then(response => {
+      componentCreator(response);
+  })
+  .catch(error => {
+      console.log(`error: ${error}`);
+  })  
+});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
